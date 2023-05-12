1f30eaa3f585e140c268b79196d1f443db25c88d
Gian Carlo Valdivia Apaza <129539002+GianCarloValdivia19@users.noreply.github.com>
2023-05-12 13:55:51 -0500

    Add files via upload

---
+++ T2ListaAlumnos/src/entidades/Asistencia.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author GIAN
+ */
+public class Asistencia {
+    private int hora;
+    private String fecha;
+    
+    
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }    
+}
---
+++ T2ListaAlumnos/src/t2listaalumnos/T2ListaAlumnos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t2listaalumnos;
+
+/**
+ *
+ * @author GIAN
+ */
+public class T2ListaAlumnos {
+
+    public static void main(String[] args) {
+
+     
+    }   
+}
